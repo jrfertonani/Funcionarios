1155fefd86d08225a23fe5dd2048bfde0d456b53
D M I <ademirjuniorfertonani@gmail.com>
2025-05-26 19:16:34 -0300

    dto response

--- back/src/main/java/back/Employees/model/DTO/employeesRequest.java
+++ back/src/main/java/back/Employees/model/DTO/employeesRequest.java
@@ -6,5 +6,5 @@ import lombok.Data;
 public class employeesRequest {
 
     private String email;
-    
+
 }
---
+++ back/src/main/java/back/Employees/model/DTO/employeesResponse.java
@@ -0,0 +1,9 @@
+package back.Employees.model.DTO;
+
+import lombok.Data;
+
+@Data
+public class employeesResponse extends employeesRequest{
+
+    private Integer id;
+}
