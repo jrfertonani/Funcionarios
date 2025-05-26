8fc6e409773f7c0e5705073a314e9584d400bc5e
D M I <ademirjuniorfertonani@gmail.com>
2025-05-26 19:13:14 -0300

    entity

---
+++ back/src/main/java/back/Employees/model/entity/Employees.java
@@ -0,0 +1,28 @@
+package back.Employees.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Data
+@Getter     @Setter
+@Entity
+public class Employees implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+
+    private String name;
+    private String email;
+    
+}
