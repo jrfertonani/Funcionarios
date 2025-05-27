eff1ac44ff1dd2508a15d0f66c296c2b4974b4a0
D M I <ademirjuniorfertonani@gmail.com>
2025-05-26 22:27:38 -0300

    Swagger completed

--- back/src/main/java/back/Employees/resource/employeesResource.java
+++ back/src/main/java/back/Employees/resource/employeesResource.java
@@ -4,6 +4,11 @@ import back.Employees.Service.employeesService;
 import back.Employees.model.DTO.employeesRequest;
 import back.Employees.model.DTO.employeesResponse;
 import back.Employees.model.entity.EmployeesEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-
+@Tag(name = "Clientes")
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/employees")
@@ -26,6 +31,12 @@ public class employeesResource {
 
 
     @PostMapping
+    @Operation(summary = "Employee registration",description = "\n" +
+            "This function is responsible for registering employees.")
+    @ApiResponse(responseCode = "200",description = "Employee registration completed successfully",
+            content = {@Content(schema = @Schema(implementation = employeesRequest.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<employeesRequest> postEmployees(@RequestBody employeesRequest request){
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -35,6 +46,11 @@ public class employeesResource {
     }
 
     @GetMapping
+    @Operation(summary = "Employee List")
+    @ApiResponse(responseCode = "200",description = "Employee registration completed successfully",
+            content = {@Content(schema = @Schema(implementation = employeesRequest.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<List<employeesResponse>> getAllEmployees(){
                 return ResponseEntity.ok().body(
                         service.getAllEmployees()
@@ -44,6 +60,12 @@ public class employeesResource {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Search by employees ID", description = "\n" +
+            "This function searches for one employee at a time 'ID'")
+    @ApiResponse(responseCode = "200",description = "Employee registration completed successfully",
+            content = {@Content(schema = @Schema(implementation = employeesRequest.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<employeesRequest> getIdEmployees(@PathVariable Integer id ){
         return ResponseEntity.ok().body(
                 service.findByIdEmployees(id)
@@ -51,6 +73,11 @@ public class employeesResource {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "change employees.",description = "This function is responsible for changing the employee record.")
+    @ApiResponse(responseCode = "200",description = "Employee registration completed successfully",
+            content = {@Content(schema = @Schema(implementation = employeesRequest.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<employeesRequest> updateEmployees(@PathVariable Integer id,
                                                           @RequestBody employeesRequest request){
         request.setId(id);
@@ -59,6 +86,11 @@ public class employeesResource {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Remove employees", description = "This function is responsible for removing employees")
+    @ApiResponse(responseCode = "200",description = "Employee registration completed successfully",
+            content = {@Content(schema = @Schema(implementation = employeesRequest.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<employeesRequest> deleteEmployees(@PathVariable Integer id ){
         service.deleteByIdEmployees(id);
         return ResponseEntity.noContent().build();
