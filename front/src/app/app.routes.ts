5b1e01b2fdef78a11d205e335d048fa42d94de49
D M I <ademirjuniorfertonani@gmail.com>
2025-05-28 20:40:23 -0300

    app.config

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { EmployeesCreateComponent } from './components/employees/employees-create/employees-create.component';
+import { EmployeesUpdateComponent } from './components/employees/employees-update/employees-update.component';
 
 export const routes: Routes = [
 
 {path:'', component: HomeComponent},
 {path:'/employees',component: EmployeesListComponent},
+{path:'/employees/create',component: EmployeesCreateComponent},
+{path:'/employees/:id',component: EmployeesUpdateComponent},
+
+
 
 ];
