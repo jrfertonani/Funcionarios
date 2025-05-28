d20806dd249a33adfc1d235ba38531b8357961e2
D M I <ademirjuniorfertonani@gmail.com>
2025-05-28 20:32:04 -0300

    components list - create - update - form

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{path:'', component: HomeComponent},
+{path:'/employees',component: EmployeesListComponent},
+
+];
