b4f6c64067828c03a255473342837fa770e9fc3a
D M I <ademirjuniorfertonani@gmail.com>
2025-05-28 20:42:00 -0300

    app.components

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './pages/template/nav/nav.component';
+import { FooterComponent } from './pages/template/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
